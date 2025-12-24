9f50a99494a0fba667aa1358f87c65f3d516a220
abimichel <abigail.michel@gov.bc.ca>
2025-12-24 12:51:03 -0800

    clean up

--- src/pmt_warehouse.Warehouse/control/Tables/watermark.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [control].[watermark] (
-
-	[source_name] varchar(100) NULL, 
-	[last_watermark] datetime2(3) NULL
-);
--- src/pmt_warehouse.Warehouse/dbo/Tables/watermark.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [dbo].[watermark] (
-
-	[source_name] varchar(100) NULL, 
-	[last_watermark] datetime2(3) NULL
-);
