665b398168d2fa44e5e4d356a5ccaab92eeaf5cd
abimichel <136123575+abimichel@users.noreply.github.com>
2026-02-03 16:28:17 -0800

    Committing 1 item from workspace 79dafa8a-b966-4240-80f7-2e9d46baa5c3

---
+++ src/Notebook_Demo_Sacrifice.Notebook/notebook-content.py
@@ -0,0 +1,46 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "201b9b7d-ca11-46b6-81e8-feeba4c99642",
+# META       "default_lakehouse_name": "permitting_lakehouse",
+# META       "default_lakehouse_workspace_id": "79dafa8a-b966-4240-80f7-2e9d46baa5c3",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "201b9b7d-ca11-46b6-81e8-feeba4c99642"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM permitting_lakehouse.monitor.pies_etl_daily_metrics LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
