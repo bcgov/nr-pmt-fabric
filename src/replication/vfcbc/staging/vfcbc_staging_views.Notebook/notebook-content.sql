54e4fe5c3ad274a6492d9f18da5f6f6ce1a19578
Jakia-S <jakia.sultana2318@yahoo.com>
2026-06-11 14:44:08 -0700

    Committing 1 item from workspace 79dafa8a-b966-4240-80f7-2e9d46baa5c3

--- src/replication/vfcbc/staging/vfcbc_staging_views.Notebook/notebook-content.sql
+++ src/replication/vfcbc/staging/vfcbc_staging_views.Notebook/notebook-content.sql
@@ -22,14 +22,21 @@
 
 -- CELL ********************
 
-CREATE OR ALTER VIEW vfcbc_staging.vfcbc_mines_auths_vw AS
+CREATE OR ALTER VIEW vfcbc_staging.vfcbc_ats_all_auths_vw AS
 SELECT
     a.JOBID,
     a.TRACKINGNUMBER,
     a.ATSLINKID,
     b.ATSAUTHORIZATIONNUMBER,
     a.ATSPROJECTNUMBER,
+    d.PROJECT_ID,
+    b.LANDSFILENUMBER,
+    d.FILE_NUMBER,
+    b.DISPOSITION_TRANSACTION_STRING,
     a.EXTERNALREFERENCE,
+    a.EXTERNALJOBNUMBER,
+    a.EXTERNALJOBNUMBER2,
+    a.MINENUMBER,
     a.BUSINESSAREA,
     a.BUSINESSAREANUMBER,
     a.OFFICE,
@@ -39,135 +46,32 @@ SELECT
     a.APPLICATIONTYPE,
     a.STATUS,
     a.STATUSSHOWNTOCLIENT,
+    d.AUTHORIZATION_STATUS_CODE,
+    f.DESCRIPTION AS AUTHORIZATION_STATUS_DESC,
+    d.ATHN_CLOSE_REASON_CODE,
+    e.DESCRIPTION AS ATHN_CLOSE_REASON_DESC,
     a.CREATEDDATE,
     a.SUBMITTEDDATE,
+    d.APPLICATION_RECEIVED_DATE,
     a.RECEIVEDDATE,
-    a.DATECOMPLETED
-FROM vfcbc_replication.vfcbc_application_vw AS a
-LEFT JOIN vfcbc_replication.vfcbc_atsnumber_vw AS b
+    d.APPLICATION_ACCEPTED_DATE,
+    d.APPLICATION_REJECTED_DATE,
+    a.DATECOMPLETED,
+    d.FIRST_NATION_START_DATE,
+    d.FIRST_NATION_COMPLETION_DATE,
+    d.TECH_REVIEW_COMPLETION_DATE,
+    d.ADJUDICATION_DATE
+FROM vfcbc_replication.vfcbc_application_vw a
+LEFT JOIN vfcbc_replication.vfcbc_atsnumber_vw b
     ON a.ATSLINKID = b.ATSLINKID
-WHERE a.BUSINESSAREA = 'Mines Competitiveness and Authorizations Division';
-
-
--- METADATA ********************
-
--- META {
--- META   "language": "sql",
--- META   "language_group": "sqldatawarehouse"
--- META }
-
--- CELL ********************
-
-CREATE OR ALTER VIEW vfcbc_staging.vfcbc_lands_auths_vw AS
-SELECT
-        a.JOBID,
-        a.TRACKINGNUMBER,
-        a.ATSLINKID,
-        b.ATSAUTHORIZATIONNUMBER,
-        a.ATSPROJECTNUMBER,
-        b.LANDSFILENUMBER,
-        b.DISPOSITION_TRANSACTION_STRING,
-        a.BUSINESSAREA,
-        a.BUSINESSAREANUMBER,
-        a.OFFICE,
-        a.APPLICANT,
-        a.SUBMITTER,
-        a.AUTHORIZATIONTYPE,
-        a.APPLICATIONTYPE,
-        a.STATUS,
-        a.STATUSSHOWNTOCLIENT,
-        a.CREATEDDATE,
-        a.SUBMITTEDDATE,
-        a.RECEIVEDDATE,
-        a.DATECOMPLETED
-    FROM vfcbc_replication.vfcbc_application_vw a
-    LEFT JOIN vfcbc_replication.vfcbc_atsnumber_vw b
-        ON a.ATSLINKID = b.ATSLINKID
-    LEFT JOIN vfcbc_replication.vfcbc_crownlandtenureapparea c
-        ON b.ATSAUTHORIZATIONNUMBER = c.ATSAUTHORIZATIONNUMBER
-    WHERE a.BUSINESSAREA = 'Crown Land Adjudication (vFCBC)';
-
--- METADATA ********************
-
--- META {
--- META   "language": "sql",
--- META   "language_group": "sqldatawarehouse"
--- META }
-
--- CELL ********************
-
-CREATE OR ALTER VIEW vfcbc_staging.vfcbc_all_auths_vw AS
-
-WITH base AS (
-    SELECT
-        a.JOBID,
-        a.TRACKINGNUMBER,
-        a.ATSLINKID,
-        b.ATSAUTHORIZATIONNUMBER,
-        a.ATSPROJECTNUMBER,
-        b.LANDSFILENUMBER,
-        b.DISPOSITION_TRANSACTION_STRING,
-        a.EXTERNALREFERENCE,
-        a.EXTERNALJOBNUMBER,
-        a.EXTERNALJOBNUMBER2,
-        a.MINENUMBER,
-        a.BUSINESSAREA,
-        a.BUSINESSAREANUMBER,
-        a.OFFICE,
-        a.APPLICANT,
-        a.SUBMITTER,
-        a.AUTHORIZATIONTYPE,
-        a.APPLICATIONTYPE,
-        a.STATUS,
-        a.STATUSSHOWNTOCLIENT,
-        a.CREATEDDATE,
-        a.SUBMITTEDDATE,
-        a.RECEIVEDDATE,
-        a.DATECOMPLETED
-    FROM vfcbc_replication.vfcbc_application_vw a
-    LEFT JOIN vfcbc_replication.vfcbc_atsnumber_vw b
-        ON a.ATSLINKID = b.ATSLINKID
-    LEFT JOIN vfcbc_replication.vfcbc_crownlandtenureapparea c
-        ON b.ATSAUTHORIZATIONNUMBER = c.ATSAUTHORIZATIONNUMBER
-),
-
-exploded AS (
-    SELECT
-        base.*,
-        LTRIM(RTRIM(s.value)) AS disposition_value
-    FROM base
-    CROSS APPLY STRING_SPLIT(base.DISPOSITION_TRANSACTION_STRING, ',') s
-)
-
-SELECT
-    JOBID,
-    TRACKINGNUMBER,
-    ATSLINKID,
-    ATSAUTHORIZATIONNUMBER,
-    ATSPROJECTNUMBER,
-    LANDSFILENUMBER,
-    CAST(disposition_value AS INT) AS DISPOSITION_TRANSACTION_SID,
-    EXTERNALREFERENCE,
-    EXTERNALJOBNUMBER,
-    EXTERNALJOBNUMBER2,
-    MINENUMBER,
-    BUSINESSAREA,
-    BUSINESSAREANUMBER,
-    OFFICE,
-    APPLICANT,
-    SUBMITTER,
-    AUTHORIZATIONTYPE,
-    APPLICATIONTYPE,
-    STATUS,
-    STATUSSHOWNTOCLIENT,
-    CREATEDDATE,
-    SUBMITTEDDATE,
-    RECEIVEDDATE,
-    DATECOMPLETED
-FROM exploded
-WHERE disposition_value IS NOT NULL
-  AND disposition_value <> ''
-  AND TRY_CAST(disposition_value AS INT) IS NOT NULL;
+LEFT JOIN vfcbc_replication.vfcbc_crownlandtenureapparea c
+    ON b.ATSAUTHORIZATIONNUMBER = c.ATSAUTHORIZATIONNUMBER
+LEFT JOIN ats_replication.ats_authorizations d
+    ON b.ATSAUTHORIZATIONNUMBER = d.AUTHORIZATION_ID
+LEFT JOIN ats_replication.ats_athn_close_reason_codes e
+    ON d.ATHN_CLOSE_REASON_CODE = e.ATHN_CLOSE_REASON_CODE
+LEFT JOIN ats_replication.ats_authorization_status_codes f 
+    ON d.AUTHORIZATION_STATUS_CODE = f.AUTHORIZATION_STATUS_CODE;
 
 -- METADATA ********************
 
